9a7b1f0d18d1c98cee18048fb572561e0f3597f7
Anthony Johnston <aj@antix.co.uk>
2020-04-16 00:00:55 +0100

    fix(gang): code to separate files

---
+++ src/Gang/GangEventHandlerBase.cs
@@ -0,0 +1,19 @@
+﻿using Gang.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace Gang
+{
+    public abstract class GangEventHandlerBase<TEvent> : IGangEventHandler
+        where TEvent : GangEvent
+    {
+        public Type EventType { get; } = typeof(TEvent);
+
+        Task IGangEventHandler.HandleAsync(GangEvent e)
+        {
+            return HandleAsync((TEvent)e);
+        }
+
+        public abstract Task HandleAsync(TEvent e);
+    }
+}
---
+++ src/Gang/IGangEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using Gang.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace Gang
+{
+    public interface IGangEventHandler
+    {
+        Type EventType { get; }
+        Task HandleAsync(GangEvent e);
+    }
+}
