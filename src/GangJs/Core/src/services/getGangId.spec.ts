fded583c6c176cd202a6690f7848771cda44e2a4
Anthony Johnston <aj@antix.co.uk>
2020-09-06 18:54:30 +0100

    fix(getGangId): add dashes so that matches standard guid serialization

--- src/GangJs/Core/src/services/getGangId.spec.ts
+++ src/GangJs/Core/src/services/getGangId.spec.ts
@@ -3,19 +3,20 @@ import { getGangId } from './getGangId';
 describe('getGangId', () => {
   it('not null', () => {
     const id = getGangId();
+    console.log(id);
 
     expect(id).not.toBeNull();
   });
 
-  it('is 32 chars', () => {
+  it('is 36 chars and dashes', () => {
     const id = getGangId();
 
-    expect(id.length).toBe(32);
+    expect(id.length).toBe(36);
   });
 
   it('looks like a guid', () => {
     const id = getGangId();
 
-    expect(id[12]).toBe('4');
+    expect(id[14]).toBe('4');
   });
 });
