3e64106ad10a4cfbdd065c53e33667abf5e08870
Anthony Johnston <aj@antix.co.uk>
2020-09-06 13:54:42 +0100

    feat(getGanId): now a guid

--- src/GangJs/Core/src/services/getGangId.spec.ts
+++ src/GangJs/Core/src/services/getGangId.spec.ts
@@ -1,8 +1,21 @@
 import { getGangId } from './getGangId';
 
-describe('Gang', () => {
-  it('can getId', () => {
+describe('getGangId', () => {
+  it('not null', () => {
     const id = getGangId();
+
     expect(id).not.toBeNull();
+  });
+
+  it('is 32 chars', () => {
+    const id = getGangId();
+
+    expect(id.length).toBe(32);
+  });
+
+  it('looks like a guid', () => {
+    const id = getGangId();
+
+    expect(id[12]).toBe('4');
   });
 });
--- src/GangJs/Core/src/services/getGangId.ts
+++ src/GangJs/Core/src/services/getGangId.ts
@@ -1,6 +1,18 @@
-export function getGangId(): string {
-  const values = new Uint8Array(16);
-  crypto.getRandomValues(values);
+export const getGangId = (function () {
 
-  return Array.from(values, (i) => ('0' + i.toString(32)).substr(-2)).join('');
-}
+  var map = [];
+
+  for (var i = 0; i < 256; i++) {
+    map[i] = (i < 16 ? '0' : '') + i.toString(16);
+  }
+
+  return () => {
+    const r = crypto
+      .getRandomValues(new Uint8Array(16));
+
+    r[6] = r[6] & 0x0f | 0x40;
+    r[8] = r[8] & 0x3f | 0x80;
+
+    return [...r.entries()].map(v => map[v[1]]).join('');
+  }
+})();
